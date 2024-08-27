0b6e2ee5f2051c96ae759fa3e9208c15ef0cb4f5
automatewithjithin <jithinjose165@yahoo.com>
2024-08-27 21:38:35 +0530

    Factorial Program

---
+++ JavaPrograms/src/main/Recursion/Factorial.java
@@ -0,0 +1,17 @@
+package src.main.Recursion;
+
+public class Factorial {
+
+    public static void main(String []args){
+        int result = factorial(5);
+        System.out.println(result);
+    }
+
+    private static int factorial(int n) {
+        if(n==1)
+            return 1;
+        return n*factorial(n-1);
+    }
+
+
+}
