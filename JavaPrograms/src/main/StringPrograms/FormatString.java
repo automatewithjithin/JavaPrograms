6e67941eb03768599fc1746c3d3d92357bbeeeba
jithin jose <jithinjosects@gmail.com>
2025-01-06 13:23:58 +0530

    format string program corrected

--- JavaPrograms/src/main/StringPrograms/FormatString.java
+++ JavaPrograms/src/main/StringPrograms/FormatString.java
@@ -5,8 +5,8 @@ public class FormatString {
         //given a string "a2b3c4d5" separate alphabet and numbers
 
         String s1="a2b3c4d5";
-        String word=s1.replaceAll("[a-z]","");
-        String numbers=s1.replaceAll("[0-9]","");
+        String word=s1.replaceAll("[^a-z]","");
+        String numbers=s1.replaceAll("[^0-9]","");
         System.out.println(word);
         System.out.println(numbers);
     }
